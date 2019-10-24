7bd4e5416c93627f034fb647c8bf6729958b75b7
rsparapa <rsparapa@mcw.edu>
2019-10-24 15:26:21 -0500

    updating demos for testing

--- btBART/demo/sim.R
+++ btBART/demo/sim.R
@@ -1,10 +1,10 @@
 ## original file is/was ...
-## /home/btuyishimire/allfiles/RESEARCH/NEWBART/TEST/A1CDATA/a1cdata_full.R
+## ~btuyishimire/allfiles/RESEARCH/NEWBART/TEST/A1CDATA/a1cdata_full.R
 library(btBART)
 library(BART3)
-library(BayesTree)
+##library(BayesTree)
 library(lmeVarComp)
-##source("/home/btuyishimire/allfiles/RESEARCH/Rfunctions/lpml.R")
+##source("~btuyishimire/allfiles/RESEARCH/Rfunctions/lpml.R")
 
 set.seed(20L)
 x <- matrix(runif(200L), 100L, 2L)
@@ -14,10 +14,10 @@ y <- 2*x1 + x2 + 10*x1*x2 + rnorm(100L)
 
 ndpost <- 1000; burn <- 100
 
-post1 <- wbart( x.train= x,
+post1 <- wbart( x.train  = x,
                 y.train  = y,   
                 ndpost   = ndpost, 
-                nskip    =  burn)
+                nskip    = burn)
 
 sigs1 <- post1$sigma[(burn+1):(burn+ndpost)]
 mus1 <- post1$yhat.train
@@ -44,21 +44,20 @@ x1 <- x[, 1L]; x2 <- x[, 2L]
 
 y <- 2*x1 + x2  + rnorm(100L)
 
-
-post1 <- wbart( x.train= x,
+post1 <- wbart( x.train  = x,
                 y.train  = y,   
                 ndpost   = ndpost, 
-                nskip    =  burn)
+                nskip    = burn)
 
 sigs1 <- post1$sigma[(burn+1):(burn+ndpost)]
 mus1 <- post1$yhat.train
 out1 <- lpml(y,mus1,sigs1)
 
-post2 <- twbarts( x.train1  = x1,
-                  x.train2  = x2,
+post2 <- twbarts( x.train1 = x1,
+                  x.train2 = x2,
                   y.train  = y,   
                   ndpost   = ndpost, 
-                  nskip    =  burn)
+                  nskip    = burn)
 
 sigs2 <- post2$sigma[(burn+1):(burn+ndpost)]
 mus2 <- post2$yhat.train
@@ -75,11 +74,10 @@ x1 <- x[, 1L]; x2 <- x[, 2L]
 
 y <- 2*x1 + x2 + 3*x1*x2 + rnorm(100L)
 
-
 post1 <- wbart( x.train  = x,
                 y.train  = y,   
                 ndpost   = ndpost, 
-                nskip    =  burn)
+                nskip    = burn)
 
 sigs1 <- post1$sigma[(burn+1):(burn+ndpost)]
 mus1 <- post1$yhat.train
@@ -89,13 +87,13 @@ post2 <- twbarts( x.train1 = x1,
                   x.train2 = x2,
                   y.train  = y,   
                   ndpost   = ndpost, 
-                  nskip    =  burn)
+                  nskip    = burn)
 
 sigs2 <- post2$sigma[(burn+1):(burn+ndpost)]
 mus2 <- post2$yhat.train
 out2 <- lpml(y,mus2,sigs2)
 
-add_test3 <<- test.additivity(x, y)
-pbf_ex3<<- exp(out1$LPML - out2$LPML)
+add_test3 <- test.additivity(x, y)
+pbf_ex3 <- exp(out1$LPML - out2$LPML)
 add_test3
 pbf_ex3
