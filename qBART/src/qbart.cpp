84ef641c3b5520b3e4ac650a8dfb6f4cacc55198
Xiao Li <xiaoli@mcw.edu>
2020-04-16 11:53:12 -0500

    gibbs cond

--- qBART/src/qbart.cpp
+++ qBART/src/qbart.cpp
@@ -23,8 +23,10 @@
 
 #ifndef NoRcpp
 
-#define TRDRAW(a, b) trdraw(a, b)
-#define TEDRAW(a, b) tedraw(a, b)
+#define TRDRAW1(a, b) trdraw1(a, b)
+#define TEDRAW1(a, b) tedraw1(a, b)
+#define TRDRAW2(a, b) trdraw2(a, b)
+#define TEDRAW2(a, b) tedraw2(a, b)
 
 RcppExport SEXP cqbart(
    //SEXP _type,          //1:wbart, 2:pbart, 3:lbart
@@ -131,8 +133,10 @@ RcppExport SEXP cqbart(
    Rcpp::NumericMatrix X1info(_X1info);
    Rcpp::NumericMatrix X2info(_X2info);
    Rcpp::NumericVector sdraw(nd+burn);
-   Rcpp::NumericMatrix trdraw(nkeeptrain,n);
-   Rcpp::NumericMatrix tedraw(nkeeptest,np);
+   Rcpp::NumericMatrix trdraw1(nkeeptrain,n);
+   Rcpp::NumericMatrix tedraw1(nkeeptest,np);
+   Rcpp::NumericMatrix trdraw2(nkeeptrain,n);
+   Rcpp::NumericMatrix tedraw2(nkeeptest,np);
 
    //random number generation
    arn gen;
@@ -164,8 +168,10 @@ RcppExport SEXP cqbart(
    }
 #else
 
-#define TRDRAW(a, b) trdraw[a][b]
-#define TEDRAW(a, b) tedraw[a][b]
+#define TRDRAW1(a, b) trdraw1[a][b]
+#define TEDRAW1(a, b) tedraw1[a][b]
+#define TRDRAW2(a, b) trdraw2[a][b]
+#define TEDRAW2(a, b) tedraw2[a][b]
 
 void cqbart(
    //int type,            //1:wbart, 2:pbart, 3:lbart
@@ -209,17 +215,21 @@ void cqbart(
    unsigned int n1, // additional parameters needed to call from C++
    unsigned int n2,
    double* sdraw,
-   double* _trdraw,
-   double* _tedraw
+   double* _trdraw1,
+   double* _tedraw1,
+   double* _trdraw2,
+   double* _tedraw2
 )
 {
    //return data structures (using C++)
    size_t nkeeptrain=nd/thin, nkeeptest=nd/thin, nkeeptreedraws=nd/thin;
-   std::vector<double*> trdraw(nkeeptrain);
-   std::vector<double*> tedraw(nkeeptest);
+   std::vector<double*> trdraw1(nkeeptrain);
+   std::vector<double*> tedraw1(nkeeptest);
+   std::vector<double*> trdraw2(nkeeptrain);
+   std::vector<double*> tedraw2(nkeeptest);
 
-   for(size_t i=0; i<nkeeptrain; ++i) trdraw[i]=&_trdraw[i*n];
-   for(size_t i=0; i<nkeeptest; ++i) tedraw[i]=&_tedraw[i*np];
+   for(size_t i=0; i<nkeeptrain; ++i) {trdraw1[i]=&_trdraw1[i*n]; trdraw2[i]=&_trdraw2[i*n];}
+   for(size_t i=0; i<nkeeptest; ++i) {tedraw1[i]=&_tedraw1[i*np]; tedraw2[i]=&_tedraw2[i*np];}
 
    //matrix to return dart posteriors (counts and probs)
    std::vector< std::vector<size_t> > varcnt1;
@@ -236,20 +246,11 @@ void cqbart(
 
    std::stringstream treess;  //string stream to write trees to
    treess.precision(10);
-   treess << nkeeptreedraws << " " << m << " " << p << endl;
+   treess << nkeeptreedraws << " " << m << " " << p1 << endl;
 
    printf("*****Calling qbart: \n");
 
    size_t skiptr=thin, skipte=thin, skiptreedraws=thin;
-/*
-   size_t skiptr,skipte,skipteme,skiptreedraws;
-   if(nkeeptrain) {skiptr=nd/nkeeptrain;}
-   else skiptr = nd+1;
-   if(nkeeptest) {skipte=nd/nkeeptest;}
-   else skipte=nd+1;
-   if(nkeeptreedraws) {skiptreedraws = nd/nkeeptreedraws;}
-   else skiptreedraws=nd+1;
-*/
 
    //--------------------------------------------------
    //print args
@@ -258,8 +259,8 @@ void cqbart(
    printf("y1,yn: %lf, %lf\n",iy[0],iy[n-1]);
    printf("x11,x1[n*p1]: %lf, %lf\n",ix1[0],ix1[n*p1-1]);
    printf("x21,x2[n*p2]: %lf, %lf\n",ix2[0],ix2[n*p2-1]);
-   if(np1) printf("xp11,xp1[np*p1]: %lf, %lf\n",ixp1[0],ixp1[np*p1-1]);
-   if(np2) printf("xp21,xp2[np*p2]: %lf, %lf\n",ixp2[0],ixp2[np*p2-1]);
+   if(np) printf("xp11,xp1[np*p1]: %lf, %lf\n",ixp1[0],ixp1[np*p1-1]);
+   if(np) printf("xp21,xp2[np*p2]: %lf, %lf\n",ixp2[0],ixp2[np*p2-1]);
    printf("*****Number of Trees: %zu\n",m);
    printf("*****Number of Cut Points for BART1: %d ... %d\n", numcut1[0], numcut1[p1-1]);
    printf("*****Number of Cut Points for BART2: %d ... %d\n", numcut2[0], numcut2[p2-1]);
@@ -287,6 +288,7 @@ void cqbart(
    int *q = new int[n];
    double *pz1 = new double[n];
    double *pt = new double[n];
+   double *st = new double[n];
    //double *svec = new double[n]; 
    
    //double *sign;
@@ -311,15 +313,16 @@ void cqbart(
    bm2.setdart(a,b,rho2,aug,dart);
 
    // dart iterations
-   std::vector<double> ivarprb1 (p,0.);
-   std::vector<size_t> ivarcnt1 (p,0);
-   std::vector<double> ivarprb2 (p,0.);
-   std::vector<size_t> ivarcnt2 (p,0);
+   std::vector<double> ivarprb1 (p1,0.);
+   std::vector<size_t> ivarcnt1 (p1,0);
+   std::vector<double> ivarprb2 (p2,0.);
+   std::vector<size_t> ivarcnt2 (p2,0);
    //--------------------------------------------------
    //temporary storage
    //out of sample fit
-   double* fhattest=0; 
-   if(np) { fhattest = new double[np]; }
+   double* fhattest1=0; 
+   double* fhattest2=0;
+   if(np) { fhattest1 = new double[np]; fhattest2 = new double[np]; }
 
    //--------------------------------------------------
    //mcmc
@@ -341,6 +344,8 @@ void cqbart(
       
       //draw bart2
       bm2.draw(sigma, gen);
+      //draw bart1
+      bm1.draw(1., gen);
 
       //draw sigma
       double rss=0.;
@@ -348,92 +353,120 @@ void cqbart(
       sigma = sqrt((nu*lambda + rss)/gen.chi_square(df));
       sdraw[i]=sigma;
 
-      //
       for (size_t k=0; k<n; k++){
+	//draw z1
 	if (q[k]==0) z1[k] = -rtnorm(-bm1.f(k), binaryOffset, 1., gen);
-	else {
-	  z1[k] = rtnorm(bm1.f(k), -binaryOffset, 1., gen);
-	}
+	else  z1[k] = rtnorm(bm1.f(k), -binaryOffset, 1., gen);
 	#ifndef NoRcpp
 	pz1[k] = R::pnorm(z1[k]+binaryOffset,0,1,1,0);
 	#else
 	pz1[k] = ::pnorm(z1[k]+binaryOffset,0,1,1,0);
 	#endif
-	if (delta[k]==0) pt[k] = pt[k]*
+	if (delta[k] == 0){
+	  if (q[k] == 1){
+	    //draw z2
+	    z2[k] = rtnorm(bm2.f(k), iy[k], sigma, gen);
+	  }
+	  #ifndef NoRcpp
+	  st[k] = R::pnorm(iy[k], bm2.f(k)+Offset, sigma, 0, 0);
+	  #else
+	  st[k] = ::pnorm(iy[k], bm2.f(k)+Offset, sigma, 0, 0);
+	  #endif
+	  pt[k] = pz1[k]*st[k]/(1-pz1[k]+pz1[k]*st[k]);
+	  //draw q
+	  #ifndef NoRcpp
+	  q[k] = R::rbinom(1, pt[k]);
+	  #else
+	  q[k] = ::rbinom(1, pt[k]);
+	  #endif
+	}
+	else q[k] = 1;
       }
 
-      //for(size_t k=0; k<n; k++) {
-	//if(type==1) {
-	  //svec[k]=iw[k]*sigma;
-	  if(delta[k]==0) z[k]= rtnorm(bm.f(k), iy[k], sigma, gen);
-	//}
-	//else {
-	  //z[k]= sign[k]*rtnorm(sign[k]*bm.f(k), -sign[k]*Offset, sigma, gen);
-	  //if(type==3) 
-	    //svec[k]=sqrt(draw_lambda_i(pow(svec[k], 2.), sign[k]*bm.f(k), 1000, 1, gen));
-	  //}
-      //}
+      
       if(i>=burn) {
          if(nkeeptrain && (((i-burn+1) % skiptr) ==0)) {
-            for(size_t k=0;k<n;k++) TRDRAW(trcnt,k)=Offset+bm.f(k);
+            for(size_t k=0;k<n;k++) {
+	      TRDRAW1(trcnt,k)=binaryOffset+bm1.f(k);
+	      TRDRAW2(trcnt,k)=Offset+bm2.f(k);
+	    }
             trcnt+=1;
          }
          keeptest = nkeeptest && (((i-burn+1) % skipte) ==0) && np;
          if(keeptest) {
-	   bm.predict(p,np,ixp,fhattest);
-            for(size_t k=0;k<np;k++) TEDRAW(tecnt,k)=Offset+fhattest[k];
-            tecnt+=1;
+	   bm1.predict(p1,np,ixp1,fhattest1);
+	   bm2.predict(p2,np,ixp2,fhattest2);
+	   for(size_t k=0;k<np;k++) {
+	     TEDRAW1(tecnt,k)=binaryOffset+fhattest1[k];
+	     TEDRAW2(tecnt,k)=Offset+fhattest2[k];
+	   }
+	   tecnt+=1;
          }
          keeptreedraw = nkeeptreedraws && (((i-burn+1) % skiptreedraws) ==0);
          if(keeptreedraw) {
-            for(size_t j=0;j<m;j++) {
-	      treess << bm.gettree(j);
-
-	      #ifndef NoRcpp
-	    ivarcnt=bm.getnv();
-	    ivarprb=bm.getpv();
-	    size_t k=(i-burn)/skiptreedraws;
-	    for(size_t j=0;j<p;j++){
-	      varcnt(k,j)=ivarcnt[j];
-	      varprb(k,j)=ivarprb[j];
-	    }
-            #else
-	    varcnt.push_back(bm.getnv());
-	    varprb.push_back(bm.getpv());
-	    #endif
-	    }
+	   for(size_t j=0;j<m;j++) {
+	     treess << bm1.gettree(j);
+             #ifndef NoRcpp
+	     ivarcnt1=bm1.getnv();
+	     ivarprb1=bm1.getpv();
+	     ivarcnt2=bm2.getnv();
+	     ivarprb2=bm2.getpv();
+	     size_t k=(i-burn)/skiptreedraws;
+	     for(size_t j=0;j<p1;j++){
+	      varcnt1(k,j)=ivarcnt1[j];
+	      varprb1(k,j)=ivarprb1[j];
+	     }
+	     for(size_t j=0;j<p2;j++){
+	      varcnt2(k,j)=ivarcnt2[j];
+	      varprb2(k,j)=ivarprb2[j];
+	     }
+             #else
+	     varcnt1.push_back(bm1.getnv());
+	     varprb1.push_back(bm1.getpv());
+	     varcnt2.push_back(bm2.getnv());
+	     varprb2.push_back(bm2.getpv());
+             #endif
+	   }
          }
       }
-
-      bm1.draw(1.,gen);
    }
    int time2 = time(&tp);
    printf("time: %ds\n",time2-time1);
    printf("trcnt,tecnt: %zu,%zu\n",trcnt,tecnt);
 
-   if(fhattest) delete[] fhattest;
-   delete[] z;
+   if(fhattest1) delete[] fhattest1; if(fhattest2) delete[] fhattest2;
+   delete[] z1; delete[] z2; delete[] pz1; delete[] pt; delete[] st;
    //delete[] svec;
-   if(type!=1) delete[] sign;
+   
 
 #ifndef NoRcpp
    //return list
    Rcpp::List ret;
-   if(type==1) ret["sigma"]=sdraw;
-   ret["yhat.train"]=trdraw;
-   ret["yhat.test"]=tedraw;
-   ret["varcount"]=varcnt;
-   ret["varprob"]=varprb;
+   ret["sigma"]=sdraw;
+   ret["y1hat.train"]=trdraw1;
+   ret["y1hat.test"]=tedraw1;
+   ret["varcount1"]=varcnt1;
+   ret["varprob1"]=varprb1;
+   ret["y2hat.train"]=trdraw2;
+   ret["y2hat.test"]=tedraw2;
+   ret["varcount2"]=varcnt2;
+   ret["varprob2"]=varprb2;
 
-   Rcpp::List xiret(xi.size());
-   for(size_t i=0;i<xi.size();i++) {
-      Rcpp::NumericVector vtemp(xi[i].size());
-      std::copy(xi[i].begin(),xi[i].end(),vtemp.begin());
-      xiret[i] = Rcpp::NumericVector(vtemp);
+   Rcpp::List xi1ret(xi1.size());
+   for(size_t i=0;i<xi1.size();i++) {
+      Rcpp::NumericVector vtemp(xi1[i].size());
+      std::copy(xi1[i].begin(),xi1[i].end(),vtemp.begin());
+      xi1ret[i] = Rcpp::NumericVector(vtemp);
+   }
+   Rcpp::List xi2ret(xi2.size());
+   for(size_t i=0;i<xi2.size();i++) {
+      Rcpp::NumericVector vtemp(xi2[i].size());
+      std::copy(xi2[i].begin(),xi2[i].end(),vtemp.begin());
+      xi2ret[i] = Rcpp::NumericVector(vtemp);
    }
 
    Rcpp::List treesL;
-   treesL["cutpoints"] = xiret;
+   treesL["cutpoints1"] = xi1ret;
    treesL["trees"]=Rcpp::CharacterVector(treess.str());
    ret["treedraws"] = treesL;
    
