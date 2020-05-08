c49d7df03f37c5c3c1feeada8695c145805e4854
Rodney Sparapani <rsparapa@mcw.edu>
2020-05-08 11:46:36 -0500

    SHAP now appears to be working in wbart example

--- BART3/R/SHAP.wbart.R
+++ BART3/R/SHAP.wbart.R
@@ -39,6 +39,38 @@ SHAP.wbart=function(object,  ## object returned from BART
         stop('the number of columns in x.train and length of cutpoints are not the same')
 
     Trees=read.trees(object$treedraws, x.train)
+    H = nrow(x.test)
+    L = dim(Trees)[1]
 
-    return(EXPVALUE(Trees, x.test, S))
+    M=P-length(S)
+    D=matrix(0, nrow=L, ncol=H)
+    N=1 ## number of terms
+
+    for(k in 1:M) {
+        C=comb(M, k, (1:P)[-S])
+        R=nrow(C)
+        N=N+R
+        for(i in 1:R)
+            D=D+EXPVALUE(Trees, x.test, c(C[i, ], S))-
+                EXPVALUE(Trees, x.test, C[i, ])
+    }
+
+    D=(D+EXPVALUE(Trees, x.test, S))/N ## S vs. emptyset
+
+    return(D)
+
+    ## D=matrix(0, nrow=L, ncol=H)
+
+    ## for(k in 1:M) {
+    ##     C=comb(M, k, (1:P)[-S])
+    ##     A=matrix(0, nrow=L, ncol=H)
+    ##     for(i in 1:nrow(C))
+    ##         A=A+EXPVALUE(Trees, x.test, c(C[i, ], S))-
+    ##             EXPVALUE(Trees, x.test, C[i, ])
+    ##     D=D+exp(lgamma(k+1)-lgamma(M+1))*A
+    ## }
+
+    ## D=D+EXPVALUE(Trees, x.test, S)/gamma(M+1) ## S vs. emptyset
+
+    ## return(D)
 }
