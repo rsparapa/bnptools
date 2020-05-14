01d4db53ada6d694a8df957a1253058d479f7675
Rodney Sparapani <rsparapa@mcw.edu>
2020-05-14 10:13:42 -0500

    re-coded in a more elegant algorithm

--- BART3/R/stratrs.R
+++ BART3/R/stratrs.R
@@ -1,6 +1,6 @@
 
 ## BART: Bayesian Additive Regression Trees
-## Copyright (C) 2017 Rodney Sparapani
+## Copyright (C) 2017-2020 Rodney Sparapani
 
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -16,41 +16,64 @@
 ## along with this program; if not, a copy is available at
 ## https://www.R-project.org/Licenses/GPL-2
 
-stratrs <- function(y, C=5, P=0)
+stratrs <- function(y, C, seed=99)
 {
+    set.seed(99)
     N <- length(y)
-    rs <- integer(N)
-
-    if(P>0) {
-        ## for continuous variables
-        Q=quantile(y, probs=(1:(P-1))/P)
-
-        for(i in 1:P) {
-            if(i<P) {
-                M <- sum(y<=Q[i])
-                rs[y<=Q[i]] <- sample(1:M, M)%%C
-            } else {
-                M <- sum(y>Q[P-1])
-                rs[y>Q[P-1] ] <- sample(1:M, M)%%C
-            }
-        }
-    } else {
-        ## for categorical variables
-        strat <- unique(sort(y))
-        
-        for(i in strat) {
-            M <- sum(y==i)
-            rs[y==i] <- sample(1:M, M)%%C
-        }
+    old.order = 1:N
+    new.order = order(y)
+    old.order = old.order[new.order]
+    strat=0
+    for(i in 1:((N%/%C)+1)) {
+        j=(i-1)*C
+        strat[j+(1:C)]=sample.int(C)
     }
-    
-    return(rs+1)
+    return(strat[order(old.order)])
 }
 
 ## set.seed(12)
-## x <- rbinom(25000, 1, 0.1)
-## a <- stratrs(x)
-## table(a, x)
-## z <- pmin(rpois(25000, 0.8), 5)
-## b <- stratrs(z)
-## table(b, z)
+## y = (c(rnorm(100), rnorm(100, 5)))
+## strat=stratrs(y, 2)
+## print(table(strat))
+## print(summary(y[strat==1]))
+## print(summary(y[strat==2]))
+
+##     set.seed(12)
+##     x <- rbinom(25000, 1, 0.1)
+##     a <- stratrs(x, 5)
+##     table(a, x)
+##     z <- pmin(rpois(25000, 0.8), 5)
+##     b <- stratrs(z, 5)
+##     table(b, z)
+
+## old/dumb version
+    ## stratrs <- function(y, C=5, P=0)
+    ## {
+    ##     N <- length(y)
+    ##     rs <- integer(N)
+
+    ##     if(P>0) {
+    ##         ## for continuous variables
+    ##         Q=quantile(y, probs=(1:(P-1))/P)
+
+    ##         for(i in 1:P) {
+    ##             if(i<P) {
+    ##                 M <- sum(y<=Q[i])
+    ##                 rs[y<=Q[i]] <- sample(1:M, M)%%C
+    ##             } else {
+    ##                 M <- sum(y>Q[P-1])
+    ##                 rs[y>Q[P-1] ] <- sample(1:M, M)%%C
+    ##             }
+    ##         }
+    ##     } else {
+    ##         ## for categorical variables
+    ##         strat <- unique(sort(y))
+
+    ##         for(i in strat) {
+    ##             M <- sum(y==i)
+    ##             rs[y==i] <- sample(1:M, M)%%C
+    ##         }
+    ##     }
+
+    ##     return(rs+1)
+    ## }
