20ee3d38bd0f3d6dcc87125963c2076dc79f9415
Rodney Sparapani <rsparapa@mcw.edu>
2020-08-10 21:25:18 -0500

    slightly more realistic example

--- BART3/demo/HDSHAP.R
+++ BART3/demo/HDSHAP.R
@@ -1,29 +1,31 @@
 
 library(BART3)
 
-f = function(x)
-    10*sin(pi*x[ , 1]*x[ , 2]) +  20*x[ , 3]
-##    10*sin(pi*x[ , 1]*x[ , 2]) + 5*x[ , 3]*x[ , 4]^2 + 20*x[ , 5]
+f=function(x)
+    10*(sin(2*pi*x[ , 1]*x[ , 2])+2*x[ , 3]-1)
 
 N = 500
-sigma = 1.0 ##y = f(x) + sigma*z where z~N(0, 1)
-P = 4       ##number of covariates
-## P = 10
-
-V = diag(P)
-V[3, 4] = 0.8
-V[4, 3] = 0.8
-## V[5, 6] = 0.8
-## V[6, 5] = 0.8
-L <- chol(V)
+sigma = 1 ##y = f(x) + sigma*z where z~N(0, 1)
+##P = 4   ##number of covariates
+P = 4
+S = 3
+C = 2 ## sample C combinations
 set.seed(12)
-x.train=matrix(rnorm(N*P), N, P) %*% L
-dimnames(x.train)[[2]] <- paste0('x', 1:P)
-round(cor(x.train), digits=2)
-x = x.train[ , 3]
+## V = diag(P)
+## V[3, 4] = 0.8
+## V[4, 3] = 0.8
+## ## V[5, 6] = 0.8
+## ## V[6, 5] = 0.8
+## L <- chol(V)
+## x.train=matrix(rnorm(N*P), N, P) %*% L
+## dimnames(x.train)[[2]] <- paste0('x', 1:P)
+## round(cor(x.train), digits=2)
+x.train=matrix(runif(N*P), nrow=N, ncol=P)
+x = x.train[ , S]
 x.train=x.train[order(x), ]
-x = x.train[ , 3]
-y.train=(f(x.train)+sigma*rnorm(N))
+x = x.train[ , S]
+## y.train=(f(x.train)+sigma*rnorm(N))
+y.train=f(x.train)
 
 B=8
 post = mc.gbart(x.train, y.train, sparse=TRUE, mc.cores=B, seed=12)
@@ -39,7 +41,7 @@ x.test = x.train
 
 ## FPD: no hot-decking
 proc.time.=proc.time()
-yhat.test=FPD(post, x.train, x.test, 3, mc.cores=B)
+yhat.test=FPD(post, x.train, x.test, S, mc.cores=B)-post$offset
 print(proc.time()-proc.time.)
 yhat.test.mean=apply(yhat.test, 2, mean)
 yhat.test.025=apply(yhat.test, 2, quantile, probs=0.025)
@@ -47,42 +49,27 @@ yhat.test.975=apply(yhat.test, 2, quantile, probs=0.975)
 
 ## HDSHAP: naive hot-decking variance
 proc.time.=proc.time()
-naive=HDSHAP(post, x.train, x.test, 3, mult.impute=20, mc.cores=B)
+naive=HDSHAP(post, x.train, x.test, S, mult.impute=20, mc.cores=B)
 print(proc.time()-proc.time.)
 
 ## HDSHAP: adjusted hot-decking variance
 proc.time.=proc.time()
-pred20=HDSHAP(post, x.train, x.test, 3, mult.impute=20, hotd.var=TRUE, mc.cores=B)
+pred20=HDSHAP(post, x.train, x.test, S, comb.draw=C, mult.impute=20, hotd.var=TRUE, mc.cores=B)
 print(proc.time()-proc.time.)
 
 proc.time.=proc.time()
-pred5=HDSHAP(post, x.train, x.test, 3, mult.impute=5, hotd.var=TRUE, mc.cores=B)
+pred5=HDSHAP(post, x.train, x.test, S, comb.draw=C, mult.impute=5, hotd.var=TRUE, mc.cores=B)
 print(proc.time()-proc.time.)
 
 proc.time.=proc.time()
-pred2=HDSHAP(post, x.train, x.test, 3, mult.impute=2, hotd.var=TRUE, mc.cores=B)
+pred2=HDSHAP(post, x.train, x.test, S, comb.draw=C, mult.impute=2, hotd.var=TRUE, mc.cores=B)
 print(proc.time()-proc.time.)
 
-## HDSHAP: adjusted hot-decking variance and imputing combinations
-proc.time.=proc.time()
-pred20.=HDSHAP(post, x.train, x.test, 3, mult.impute=20, hotd.var=TRUE,
-              comb.impute=1, mc.cores=B)
-print(proc.time()-proc.time.)
-
-par(mfrow=c(2, 1))
-for(i in 1:2) {
-    if(i==1) {
-        plot(x, 20*x, type='l', xlab='x3', ylab='f(x3)', lwd=2,
-                     xlim=c(-0.5, 0.5), ylim=c(-15, 15))
-        } else {
-            plot(x, 20*x, type='l', xlab='x3', ylab='f(x3)', lwd=2,
-                 xlim=c(0.5, 2.5), ylim=c(25, 50))
+        plot(x, f(cbind(0, 0, x)), type='l', xlab='x3', ylab='f(x3)', lwd=2,
+                     ylim=c(-5, 5), xlim=c(0.2, 0.8))
         legend('topleft', lwd=2,
-               ##col=0:7, lty=0:7,
-               col=1:7, lty=1:7,
-           legend=c(##'Method 2',
-               'True', 'FPD', 'Naive 20', '20', '5', '2', '20+'))
-        }
+               col=1:6, lty=1:6,
+           legend=c('True', 'FPD', 'Naive 20', '20', '5', '2'))
     lines(x, yhat.test.mean, col=2, lwd=2)
     lines(x, yhat.test.025, col=2, lty=2, lwd=2)
     lines(x, yhat.test.975, col=2, lty=2, lwd=2)
@@ -97,10 +84,5 @@ for(i in 1:2) {
     lines(x, pred2$yhat.test.mean, col=6, lwd=2)
     lines(x, pred2$yhat.test.lower, col=6, lty=6, lwd=2)
     lines(x, pred2$yhat.test.upper, col=6, lty=6, lwd=2)
-    lines(x, pred20.$yhat.test.mean, col=7, lwd=2)
-    lines(x, pred20.$yhat.test.lower, col=7, lty=7, lwd=2)
-    lines(x, pred20.$yhat.test.upper, col=7, lty=7, lwd=2)
-}
-par(mfrow=c(1, 1))
 dev.copy2pdf(file='HDSHAP.pdf')
 
