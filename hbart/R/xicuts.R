ce7cb4f04be3e4c46c570c253e1928f16d0349c4
rsparapa <rsparapa@mcw.edu>
2021-01-25 13:45:29 -0600

    updating license

--- hbart/R/xicuts.R
+++ hbart/R/xicuts.R
@@ -1,20 +1,20 @@
-## Copyright (C) 2012-2019 Matthew T. Pratola, Robert E. McCulloch,
+## Copyright (C) 2012-2021 Matthew T. Pratola, Robert E. McCulloch,
 ##                         Hugh A. Chipman and Rodney A. Sparapani
 
-## This file is part of BART.
+## This file is part of hbart.
 ## xicuts.R
 
-## BART is free software: you can redistribute it and/or modify
-## it under the terms of the GNU Affero General Public License as published by
-## the Free Software Foundation, either version 3 of the License, or
+## hbart is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 2 of the License, or
 ## (at your option) any later version.
 
-## BART is distributed in the hope that it will be useful,
+## hbart is distributed in the hope that it will be useful,
 ## but WITHOUT ANY WARRANTY; without even the implied warranty of
 ## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU Affero General Public License for more details.
+## GNU General Public License for more details.
 
-## You should have received a copy of the GNU Affero General Public License
+## You should have received a copy of the GNU General Public License
 ## along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ## Author contact information
