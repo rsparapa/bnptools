18e7f3522ca17da0461951f7f46f6e4b397effea
rsparapa <rsparapa@mcw.edu>
2024-05-18 15:44:00 -0500

    removing comments

--- BART3/demo/nci-topo.R
+++ BART3/demo/nci-topo.R
@@ -3,9 +3,6 @@ library(BART3)
 
 data(NCItopo)
 
-##NCItopo <- read.csv(file = 'NCI-topo.csv')
-##save(NCItopo, file = '~/git/bnptools/BART3/data/NCItopo.rda')
-
 table(NCItopo$Potency) 
 
 NCItopo$Potency[NCItopo$Potency == 2] <- 1 ## combine 1:2
@@ -17,7 +14,7 @@ table(NCItopo$Potency)
 H = 10                          ## number of trees
 T = 20                          ## number of steps
 x.train <- NCItopo[ , -c(1, P)] ## the first column is an id
-                                ## and the last col is y
+                                ## and the last col is yt 
 
 sparse <- tsvs(x.train, NCItopo$Potency, keepevery = 1, ntree = H, T = T,
                type = 'pbart',
