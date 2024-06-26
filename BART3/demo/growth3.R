dcd9d0214049a877893a09930162c1a588296f04
rsparapa <rsparapa@mcw.edu>
2024-06-26 10:36:11 -0500

    finalizing examples for workshop

--- BART3/demo/growth3.R
+++ BART3/demo/growth3.R
@@ -3,81 +3,99 @@ options(mc.cores=8)
 library(BART3)
 
 data(bmx)
-bmx$RIDRETH2=factor(bmx$RIDRETH2)
-str(bmx)
-summary(bmx$RIDAGEEX)
-x.train=bmx[ , -(1:2)]
-print(cor(bmx$BMXHT, x.train[ , c(2, 4)])^2)
+
+strata <- stratrs(bmx$BMXHT, 5, seed = 10)
+train <- (strata<5)
+x.train=bmx[train, -(1:2)]
+x.test =bmx[!train, -(1:2)]
 
 file.='growth1-fit1.rds'
 if(!file.exists(file.)) {
-    fit1 = mc.gbart(x.train, bmx$BMXHT, seed=21)
+    ## one long chain: single thread computing
+    ## set.seed(21)
+    ## fit1 = gbart(x.train, bmx$BMXHT[train], x.test=x.test) 
+    ## multiple shorter chains: parallel multi-threading
+    fit1 = mc.gbart(x.train, bmx$BMXHT[train], x.test=x.test, seed=21)
     saveRDS(fit1, file.)
 } else { fit1=readRDS(file.) }
-print(cor(bmx$BMXHT, fit1$yhat.train.mean)^2)
+print(c(train.Rsqr = cor(bmx$BMXHT[train], fit1$yhat.train.mean)^2))
+print(c(test.Rsqr = cor(bmx$BMXHT[!train], fit1$yhat.test.mean)^2))
 
-pdf(file='growth1-zep1.pdf')
-col.=c(4, 2) ## males=blue, females=red
-plot(fit1$yhat.train.mean, bmx$BMXHT, asp=1,
-     pch='.', col=col.[bmx$RIAGENDR],
-     xlab='Predicted Height (cm)',
-     ylab='Observed Height (cm)')
-abline(b=1, a=0, col=8)
-dev.off()
+age. <- 2:17
+x.test. <- cbind(0, age.) ## column 1: sex, column 2: age
+L <- nrow(x.test.)
+x.test. <- rbind(x.test., x.test.)
+x.test.[ , 1] <- rep(1:2, each = L)
 
-pdf(file='growth1-fit1.pdf')
-plot(bmx$RIDAGEEX, fit1$yhat.train.mean,
-     pch='.', col=col.[bmx$RIAGENDR],
+file.='growth1-fit2.rds'
+if(!file.exists(file.)) {
+    ## set.seed(22)
+    ## fit2 = gbart(x.train[ , 1:2], bmx$BMXWT[train], x.test=x.test.) 
+    fit2 = mc.gbart(x.train[ , 1:2], bmx$BMXWT[train], x.test=x.test., seed=22)
+    saveRDS(fit2, file.)
+} else { fit2=readRDS(file.) }
+print(c(train.Rsqr = cor(bmx$BMXWT[train], fit2$yhat.train.mean)^2))
+
+(x.test. <- cbind(x.test., fit2$yhat.test.mean))
+print(system.time(pred2 <- FPD(fit1, x.test., S = 1:3))) ## conditional means
+
+F.test <- (!train & bmx$RIAGENDR == 2)
+plot(bmx$RIDAGEEX[F.test], bmx$BMXHT[F.test],
+     pch=1, col=8,
+     cex = log2(bmx$RIDAGEEX[F.test])/2,
      ylab='Height (cm)',
      xlab='Age (yr)')
-dev.off()
+lines(age., pred2$yhat.test.mean[L+1:L], col=2, lwd=2)
+lines(age., pred2$yhat.test.lower[L+1:L], col=2, lwd=2, lty=2)
+lines(age., pred2$yhat.test.upper[L+1:L], col=2, lwd=2, lty=2)
+##dev.copy2pdf(file='growth1-cmF.pdf')
 
-(N=length(bmx$BMXHT))
-K=16 ##K=64
-(age=seq(2, 18, length.out=K+1)[1:K])
-x.test=cbind(rep(1:2, each=K), age)
-print(x.test)
+M.test <- (!train & bmx$RIAGENDR == 1)
+plot(bmx$RIDAGEEX[M.test], bmx$BMXHT[M.test],
+     pch=1, col=8,
+     cex = log2(bmx$RIDAGEEX[M.test])/2,
+     ylab='Height (cm)',
+     xlab='Age (yr)')
+lines(age., pred2$yhat.test.mean[1:L], col=4, lwd=2)
+lines(age., pred2$yhat.test.lower[1:L], col=4, lwd=2, lty=2)
+lines(age., pred2$yhat.test.upper[1:L], col=4, lwd=2, lty=2)
+##dev.copy2pdf(file='growth1-cmM.pdf')
 
-file.='growth2-fit0.rds'
+file. <- "growth3-pred.t.RDS" 
 if(!file.exists(file.)) {
-    fit0 = mc.gbart(x.train[ , -(3:4)], bmx$BMXWT, x.test, seed=21)
-    saveRDS(fit0, file.)
-} else { fit0=readRDS(file.) }
-print(cor(bmx$BMXWT, fit0$yhat.train.mean)^2)
-
-P=ncol(fit1$x.train)
-x.test = fit1$x.train[1:(2*K), ]
-x.test[ , 1]=rep(1:2, each=K)
-x.test[ , 2]=age
-x.test[ , P]=fit0$yhat.test.mean
-print(x.test[ , c(1, 2, P)])
-
-pred1 = FPDK(fit1, x.test, c(1, 2, P), kern.var=FALSE)
-pred2 = FPDK(fit1, x.test, c(1, 2, P)) ## kern.var=TRUE default
-
-pdf(file='growth3-FPDK.pdf')
-
-par(mfrow=c(1, 2))
-plot(bmx$RIDAGEEX, fit1$yhat.train.mean,
-     pch='.', col=col.[bmx$RIAGENDR], type='n',
-     ylab='Height (cm)', xlab='Age (yr)', sub='kern.var=FALSE')
-for(i in 1:2) {
-    lines(age, pred1$yhat.test.mean[(i-1)*K+1:K], col=col.[i])
-    lines(age, pred1$yhat.test.lower[(i-1)*K+1:K],
-          lty=2, col=col.[i], lwd=2)
-    lines(age, pred1$yhat.test.upper[(i-1)*K+1:K],
-          lty=2, col=col.[i], lwd=2)
+    print(system.time(pred.t <- SHAP(fit1, x.test.[1:L, 2], S = 2)))
+    saveRDS(pred.t, file.) 
+} else {
+    pred.t <- readRDS(file.) 
 }
 
-plot(bmx$RIDAGEEX, fit1$yhat.train.mean,
-     pch='.', col=col.[bmx$RIAGENDR], type='n',
-     ylab='', xlab='Age (yr)', sub='kern.var=TRUE')
-for(i in 1:2) {
-    lines(age, pred2$yhat.test.mean[(i-1)*K+1:K], col=col.[i])
-    lines(age, pred2$yhat.test.lower[(i-1)*K+1:K],
-          lty=2, col=col.[i], lwd=2)
-    lines(age, pred2$yhat.test.upper[(i-1)*K+1:K],
-          lty=2, col=col.[i], lwd=2)
+pred.t$yhat.test.mean <- pred.t$yhat.test.mean+fit1$offset
+
+file. <- "growth3-pred.w.RDS" 
+if(!file.exists(file.)) {
+    print(system.time(pred.w <- SHAP(fit1, x.test.[ , 3], S = 3)))
+    saveRDS(pred.w, "growth3-pred.w.RDS")
+} else {
+    pred.w <- readRDS(file.) 
 }
-par(mfrow=c(1, 1))
-dev.off()
+
+file. <- "growth3-pred.u.RDS" 
+if(!file.exists(file.)) {
+    print(system.time(pred.u <- SHAP(fit1, 1:2, S = 1)))
+    saveRDS(pred.u, "growth3-pred.u.RDS")
+} else {
+    pred.u <- readRDS(file.) 
+}
+
+plot(bmx$RIDAGEEX, bmx$BMXHT, type = 'n',
+     ylab='Height (cm)', xlab='Age (yr)')
+lines(age., pred2$yhat.test.mean[L+1:L], col = 2, lwd = 2)
+lines(age., pred2$yhat.test.mean[1:L], col = 4, lwd = 2)
+points(age., pred.t$yhat.test.mean)
+points(age., pred.u$yhat.test.mean[2]+pred.t$yhat.test.mean+pred.w$yhat.test.mean[L+1:L], col = 2, pch = 25)
+points(age., pred.u$yhat.test.mean[1]+pred.t$yhat.test.mean+pred.w$yhat.test.mean[1:L], col = 4, pch = 24)
+legend('topleft', legend = c('FPD Conditional Means', 'SHAP: age-only', 
+'SHAP: age, F, weight', 'SHAP: age, M, weight'),
+       pch = c(32, 1, 25, 24), col = c(0, 1, 2, 4))
+dev.copy2pdf(file='growth3-SHAP.pdf')
+
