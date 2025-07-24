bc6dfb71b467470fb91b03156bf28c879b228c70
rsparapa <rsparapa@mcw.edu>
2025-07-24 09:32:09 -0500

    updating to latest and greatest

--- BART3/R/Rhat.R
+++ BART3/R/Rhat.R
@@ -1,6 +1,6 @@
 
 ## BART: Bayesian Additive Regression Trees
-## Copyright (C) 2024 Robert McCulloch and Rodney Sparapani
+## Copyright (C) 2024-2025 Robert McCulloch and Rodney Sparapani
 ## Rhat.R
 
 ## This program is free software; you can redistribute it and/or modify
@@ -32,8 +32,7 @@ oldRhat = function(y, C) {
 
 splitRhat = function(y, C) {
     m = C*2
-    l <- length(y)
-    n = l/m
+    n = length(y)/m
     x = rep(1:m, each=n)
     fit = lm(y~factor(x))
     a = anova(fit)
@@ -42,54 +41,24 @@ splitRhat = function(y, C) {
     V = ((n-1)*W+B)/n
     Rhat = sqrt(V/W)
 
-    L <- l-1
-    if((L%%2) == 0) L <- L-1
-    L <- min(c(99, L))
-    for(j in 1:m) {
-        i <- which(x == j)
-        rho. <- var(y[i])*c(acf(y[i], lag.max=L, plot=FALSE)$acf)[-1]
-        if(j == 1) {
-            L <- length(rho.)
-            if((L%%2) == 0) L <- L-1
-            rho <- rho.[1:L]
-        } else { rho <- cbind(rho, rho.) }
-    }
-    rho <- 1-(W-apply(rho, 1, mean))/V
-    k <- L%/%2
-    P <- 0
-    for(i in 1:k) {
-        j <- 2*i-1
-        P[i] <- rho[j]+rho[j+1]
-    }
-    if(min(P)<0) k <- max(1, min(which(P<0)))-1
-
-    if(k == 0) {
-        tau <- 1
-        P <- 0
-    } else {
-        P <- P[1:k]
-        tau <- -1+2*sum(P)
+    t = 0
+    rho = 1
+    while(t<2 || rho[t]>0 || rho[t-1]>0) {
+        t = t+1
+        rho[t] = 0
+        for(j in 1:m) {
+            a = acf(y[x==j], lag.max=t, plot=FALSE)
+            rho[t] = rho[t]+a$acf[t+1, 1, 1]/m
+        }
+        if(t>1 && rho[t]<0 && rho[t-1]<0)
+            rho[(t-1):t] = c(0, 0)
     }
 
-    ## t = 0
-    ## rho = 1
-    ## while(t<2 || rho[t]>0 || rho[t-1]>0) {
-    ##     t = t+1
-    ##     rho[t] = 0
-    ##     for(j in 1:m) {
-    ##         a = acf(y[x==j], lag.max=t, plot=FALSE)
-    ##         rho[t] = rho[t]+a$acf[t+1, 1, 1]/m
-    ##     }
-    ##     if(t>1 && rho[t]<0 && rho[t-1]<0)
-    ##         rho[(t-1):t] = c(0, 0)
-    ## }
-
-    ## rho.t = 1 - (W-rho)/V
-    ## tau = 1 + 2*sum(rho.t)
-
+    rho.t = 1 - (W-rho)/V
+    tau = 1 + 2*sum(rho.t)
     Seff = n*m/tau
     return(list(splitRhat=Rhat, splitSeff=Seff, splitSpct=1/tau,
-                B=B, W=W, V=V, rho=rho, P.t = P))
+                B=B, W=W, V=V, rho=rho))
 }
 
 maxRhat = function(y, C) {
@@ -109,7 +78,7 @@ maxRhat = function(y, C) {
     split.rho=a$rho
     folded.rho=b$rho
     k=min(length(split.rho), length(folded.rho))
-    ##rho=pmax(split.rho[1:k], folded.rho[1:k])
+    rho=pmax(split.rho[1:k], folded.rho[1:k])
     ## if(acfPlot) {
     ##     k=length(split.rho)
     ##     plot(0:(k-1), split.rho, ylim=c(-1, 1))
@@ -117,8 +86,5 @@ maxRhat = function(y, C) {
     ##     plot(0:(k-1), folded.rho, ylim=c(-1, 1))
     ## }
     return(list(maxRhat=maxRhat, minSeff=minSeff, minSpct=minSeff/l,
-                splitSeff = a$splitSeff, foldedSeff = b$splitSeff,
-                splitRhat=a$splitRhat, foldedRhat=b$splitRhat,
-                splitrho=split.rho, foldedrho=folded.rho,
-                splitP.t = a$P, foldedP.t = b$P)) ## rho=rho)) 
+                split.rho=split.rho, folded.rho=folded.rho, rho=rho)) 
 }
